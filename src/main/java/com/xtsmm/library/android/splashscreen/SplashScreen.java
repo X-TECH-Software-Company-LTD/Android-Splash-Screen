dc3cd8153a21fdb62211c2f0ad68890600856b1f
Saintpetre <50192313+Saintpetre@users.noreply.github.com>
2020-11-22 16:38:52 +0630

    1

---
+++ src/main/java/com/xtsmm/library/android/splashscreen/SplashScreen.java
@@ -0,0 +1,19 @@
+package com.xtsmm.library.android.splashscreen;
+
+import android.app.Activity;
+import android.widget.RelativeLayout;
+
+public class SplashScreen {
+
+    Activity activity;
+
+    public SplashScreen(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void show(){
+
+        
+
+    }
+}
