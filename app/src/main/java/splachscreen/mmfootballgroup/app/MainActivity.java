2de1713d00624084de82856dffa3147939b3e7ee
Saintpetre <50192313+Saintpetre@users.noreply.github.com>
2020-11-24 16:01:12 +0630

    3

--- app/src/main/java/splachscreen/mmfootballgroup/app/MainActivity.java
+++ app/src/main/java/splachscreen/mmfootballgroup/app/MainActivity.java
@@ -19,11 +19,11 @@ public class MainActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_main);
 
 
-        setContentView(new SplashScreenWithGif(this).defaultCreate("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/405d156a-497c-4d3e-9e1f-1ffc1d7d13a3/db4ewyw-b28ff912-78f6-4283-a562-08ef72b31ad2.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNDA1ZDE1NmEtNDk3Yy00ZDNlLTllMWYtMWZmYzFkN2QxM2EzXC9kYjRld3l3LWIyOGZmOTEyLTc4ZjYtNDI4My1hNTYyLTA4ZWY3MmIzMWFkMi5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.-odMwG0-kPhfbx242XMZynwrIupnlz2bL39orv2m0B0",
+        setContentView(new SplashScreenWithGif(this).defaultCreate("https://1.bp.blogspot.com/-r5Aj9YbCQQk/UF80dyv17SI/AAAAAAAAEZU/aXoSQPybltI/s1600/Beautiful+3D+hd+nature+wallpaper+mobile+phones++Download++animated+gif+pictures+and+wallpapers++smartphone+phone+wallpapers+backgrounds+images+photos+gif+pictures++moon+water+reflexive.gif",
                 null,
-                "https://media1.tenor.com/images/36d8ee2a8862438f273752d376b99ec3/tenor.gif?itemid=15216980",
-                null,null,"present",null,
-                null,null));
+                "https://images.squarespace-cdn.com/content/v1/5d57c81ebfd83300010c568e/1568881657624-64DAIKETTWGNJM5OXURM/ke17ZwdGBToddI8pDm48kNiEM88mrzHRsd1mQ3bxVct7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0uyjK65ACIS1Ro5Gtg_5NxawvmiEeEPz-p1PBnM1Qfd0YvLs_GGCwkX6Y6WN_obWbw/VI_LOGO_SYMBOL_VARIATION.gif",
+                "Myanamr Application","#ff00ff","present","#ffffff",
+                "Version 1.1.0","#ffffff"));
 
         /*setContentView(new SplashScreenWithGif(this).noLogoCreate("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/405d156a-497c-4d3e-9e1f-1ffc1d7d13a3/db4ewyw-b28ff912-78f6-4283-a562-08ef72b31ad2.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNDA1ZDE1NmEtNDk3Yy00ZDNlLTllMWYtMWZmYzFkN2QxM2EzXC9kYjRld3l3LWIyOGZmOTEyLTc4ZjYtNDI4My1hNTYyLTA4ZWY3MmIzMWFkMi5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.-odMwG0-kPhfbx242XMZynwrIupnlz2bL39orv2m0B0",
                 null,
